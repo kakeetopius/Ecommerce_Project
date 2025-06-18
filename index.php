180c186df78ca7b99f56b2597472a196cc7e3344
kakeetopius <pius.kakeeto@strathmore.edu>
2025-06-18 11:13:20 +0300

    Starting project

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
-function tester()
-{
-    return -1;
-}
+<!Doctype html>
+<html>
+
+<head>
+    <title>Test Page</title>
+</head>
+
+<body>
+    <h1>It Works!!</h1>
+    <?php
+        function work() {
+            echo "<h3>Even php works!!!</h3>";
+        }
+
+        work();
+    ?>
+</body>
+
+</html>
