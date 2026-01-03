68d6459fa53fc6dee3c78f6c2396a5b7f1897923
kakeetopius <pius.kakeeto@strathmore.edu>
2026-01-03 19:16:00 +0300

    Update docker files.

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-// start session
-// check for page from get array
-// if not set go to homepage
-// get page and switch to the right Controller
-// paramters will be passed by the get array or post array
-// or session array
-// the controllers will include the data and the data 
-// will be loaded to the pages with the view pages and
-// sent back
-// If async json is sent back for js to load in the page.
-
-//setup the session information
-
 $received_url = $_SERVER['REQUEST_URI'];
 
 if ($received_url === "/") {
